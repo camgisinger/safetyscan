34feb7908840f3d884e1c00790239c94901f75d5
camgisinger <camgisinger@icloud.com>
2026-05-27 09:05:35 +1000

    fix middleware - remove deprecated createMiddlewareClient

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,8 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-  await supabase.auth.getSession()
-  return res
+  return NextResponse.next()
 }
 
 export const config = {
